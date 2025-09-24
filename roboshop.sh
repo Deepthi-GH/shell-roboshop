d2eddc472d49d8f7bb39ca4088edd5416d322063
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-24 21:54:32 +0800

    instance automate

--- roboshop.sh
+++ roboshop.sh
@@ -6,11 +6,11 @@ for INSTANCE in $@
 do 
   INSTANCES_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]' --query 'Instances[0].InstanceId' --output text)
 
-if [ $INSTANCE != "frontend"]
+if [ $INSTANCE != "frontend" ]
 then
-   IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test2}]' --query 'Instances[0].PrivateIpAddress' --output text)
+   IP=$(aws ec2 describe-instances --instance-ids INSTANCES_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 else
-    IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test2}]' --query 'Instances[0].PublicIpAddress' --output text)
+   IP=$(aws ec2 describe-instances --instance-ids INSTANCES_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 fi
 echo "$INSTANCE :$IP"
 done

02919b4125175238cbc3a769c3bedba50e232340
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-25 09:46:52 +0800

    catalogue

---
+++ catalogue.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+LOGS_FOLDER=/var/log/shell-roboshop
+SCRIPT_NAME=$( echo $0|cut -d "." -f1 )
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+MONGODB_HOST=mongodb.deepthi.cloud
+mkdir -p $LOGS_FOLDER
+echo "script started at: $(date)" |tee -a $LOG_FILE
+if [ $USERID -ne 0 ] 
+then
+    echo "error::please run this script with root priviliges"
+    exit 1
+fi
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ] 
+then
+    echo -e "error::$2 installation $R failed $N"|tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$2 installation is $Y success $N"|tee -a $LOG_FILE
+fi    
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "disabling nodejs"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enabling nodejs"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "installing nodejs"
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "user is roboshop"
+mkdir /app  &>>$LOG_FILE
+VALIDATE $? "created /app directory"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzipping the code"
+cd /app 
+npm install &>>$LOG_FILE
+VALIDATE $? "downloading dependencies"
+cp catalogue.servive /etc/systemd/system/catalogue.service
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "daemon reload"
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "enable catalogue"
+systemctl start catalogue &>>$LOG_FILE
+VALIDATE $? "start catalogue"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "installing mongodb client"
+mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "load products data"
+mongosh --host MONGODB-SERVER-IPADDRESS &>>$LOG_FILE
+
+show dbs &>>$LOG_FILE
+use catalogue &>>$LOG_FILE
+show collections &>>$LOG_FILE
+db.products.find() &>>$LOG_FILE
+
+
+
+
+
+

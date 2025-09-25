648de6401590db7073d28cacd1c328f006889de1
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-25 23:09:18 +0800

    rabbitmq

---
+++ rabbitmq.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+LOGS_FOLDER=/var/log/shell-roboshop
+SCRIPT_NAME=$( echo $0|cut -d "." -f1 )
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+MONGODB_HOST=mongodb.deepthi.cloud
+SCRIPT_DIR=$(pwd)
+START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+echo "script started at: $(date)" |tee -a $LOG_FILE
+if [ $USERID -ne 0 ] 
+then
+    echo "error::please run this script with root priviliges"
+    exit 1
+fi
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ] 
+then
+    echo -e "error::$2 ...  $R FAILED $N"|tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$2 ... $Y SUCCESS $N"|tee -a $LOG_FILE
+fi    
+}
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+VALIDATE $? "copying repo file"
+
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VALIDATE $? "installing rabbitmq"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "enabling rabbitmq"
+systemctl start rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "starting rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123  &>>$LOG_FILE
+VALIDATE $? "adding user"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>$LOG_FILE
+VALIDATE $? "setting permissions"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo -e "script executed at : $Y $TOTAL_TIME seconds"
+

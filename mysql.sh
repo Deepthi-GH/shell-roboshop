72ddcee37f950d06064db383b807fed1ecc92a24
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-25 22:43:06 +0800

    redis

--- mysql.sh
+++ mysql.sh
@@ -29,20 +29,5 @@ else
     echo -e "$2 ... $Y SUCCESS $N"|tee -a $LOG_FILE
 fi    
 }
-dnf module disable redis -y &&>$LOG_FILE
-VALIDATE $? "disabling redis"
-dnf module enable redis:7 -y &&>$LOG_FILE
-VALIDATE $? "enabling redis"
-dnf install redis -y &&>$LOG_FILE
-VALIDATE $? "installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-
-systemctl enable redis &&>$LOG_FILE
-VALIDATE $? "enabling redis"
-
-systemctl start redis &&>$LOG_FILE
-VALIDATE $? "starting redis"
-END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME - $START_TIME))
-echo -e "script executed at : $Y $TOTAL_TIME seconds"
+dnf install mysql-server -y &>>$LOG_FILE

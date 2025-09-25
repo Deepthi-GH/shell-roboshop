c321c88f8443bfe58995321281514aea02d8ea32
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-25 15:43:46 +0800

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -22,10 +22,10 @@ VALIDATE()
 {
     if [ $1 -ne 0 ] 
 then
-    echo -e "error::$2 ...  $R failed $N"|tee -a $LOG_FILE
+    echo -e "error::$2 ...  $R FAILED $N"|tee -a $LOG_FILE
     exit 1
 else
-    echo -e "$2 ... $Y success $N"|tee -a $LOG_FILE
+    echo -e "$2 ... $Y SUCCESS $N"|tee -a $LOG_FILE
 fi    
 }
 

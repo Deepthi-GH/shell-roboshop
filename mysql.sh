4db85d451e6c16d52fdd17f8aa734ca3f3c68a81
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-25 22:48:16 +0800

    mysql-server

--- mysql.sh
+++ mysql.sh
@@ -31,3 +31,13 @@ fi
 }
 
 dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "installing mysql-server"
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "enabling mysql-server"
+systemctl start mysqld &>>$LOG_FILE 
+VALIDATE $? "starting mysql-server"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+VALIDATE $? "changing root-passwd"
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo -e "script executed at : $Y $TOTAL_TIME seconds"
